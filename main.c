ebbf11ee86ee01fd329b92988b58b441b30195a6
Tarek <mcu.is_great@zoho.com>
2026-05-30 13:03:28 +0300

    just blink to make sure the mcu is running

--- main.c
+++ main.c
@@ -4,13 +4,19 @@
  * Created: 5/30/2026 12:30:06 PM
  *  Author: Trix
  */ 
-
+#define F_CPU 16000000
 #include <xc.h>
+#include <util/delay.h>
 
 int main(void)
 {
+	DDRC=0x3F;
+	PORTC=0x1;
+
     while(1)
     {
         //TODO:: Please write your application code 
+		PORTC ^= 1 << 1;
+		_delay_ms(500);
     }
 }

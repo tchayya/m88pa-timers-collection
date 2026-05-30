493bdc1a5dfde35321f80e1a4420d0aa1c434c02
Tarek <mcu.is_great@zoho.com>
2026-05-30 12:41:48 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 5/30/2026 12:30:06 PM
+ *  Author: Trix
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
